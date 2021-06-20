dce3cf23c83fba58ee266970e25dd3b437a026de
Nabil <rousani@gmail.com>
2021-06-19 18:01:00 -0700

    first

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+var React = require('react');
+var ReactDom = require("react-dom");
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+var name = 'nabil';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+ReactDom.render(
+  <div>
+    <h1> Hello World. My name is {name}</h1>
+    <ol>
+      <li> 123 </li>
+      <li> 456 </li>
+    </ol>
+  </div>
+  , document.getElementById("root"));
