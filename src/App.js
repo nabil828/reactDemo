7de8a265462ef69c7d2ed4de344abc57a25de06a
Nabil <rousani@gmail.com>
2021-06-19 21:30:43 -0700

    props

--- src/App.js
+++ src/App.js
@@ -3,11 +3,24 @@ import Rest from "./Rest.js"
 
 import './App.css';
 
+
+function Card(props){
+  return (
+    <>
+      <h1> {props.name} </h1>
+      <ul>
+        <li> Tempreture: {props.Tempreture}</li>
+        <li> Description: {props.description}</li>
+      </ul>
+    </>
+  );
+}
 function App() {
   return (
     <div>
       <Heading/>
-      <Rest/>
+      <Card name="Vancouver" Tempreture="25.2" description="Sunny"/>
+      <Card name="Tokyo" Tempreture="30.2" description="scorching"/>
     </div>
   );
 }
--- src/Heading.js
+++ src/Heading.js
@@ -2,7 +2,7 @@
 
 
 function Heading(){
-  return <h1> HEADING </h1>;
+  return <h1> Nabil's Weather Web service </h1>;
 }
 
 export default Heading;
